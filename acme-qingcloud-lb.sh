9323b7af6ed8d3388662c3df639ce25f496c5862
fivesmallq <fivesmallq@gmail.com>
2018-12-11 12:15:40 +0800

    refactor code

--- acme-qingcloud-lb.sh
+++ acme-qingcloud-lb.sh
@@ -1,5 +1,12 @@
 trap "exit" INT
 
+__red() {
+  if [ "${__INTERACTIVE}${ACME_NO_COLOR:-0}" = "10" -o "${ACME_FORCE_COLOR}" = "1" ]; then
+    printf '\033[1;31;40m%b\033[0m' "$1"
+    return
+  fi
+  printf -- "%b" "$1"
+}
 _usage() {
   __red "$@" >&2
   printf "\n" >&2
@@ -24,30 +31,7 @@ _exists() {
   return $ret
 }
 
-main() {
-config_file=""
-while [ ${#} -gt 0 ]; do
-  case "${1}" in
-    --help | -h)
-      showhelp
-      return
-      ;;
-    --version | -v)
-      version
-      return
-      ;;
-    --config | -c)
-      config_file="$2"
-      shift
-      ;;
-    *)
-      _err "Unknown parameter : $1"
-      return 1
-      ;;
-  esac
-  shift 1
-done
-
+_check_deps() {
 if ! _exists ~/.acme.sh/acme.sh; then
   echo "installing acme.sh..."
   curl  https://get.acme.sh | sh
@@ -74,7 +58,143 @@ if ! _exists jq; then
     ls -la $JQ
   fi
 fi
+}
 
+_issue() {
+  json="$1"
+  echo $json
+  if [ -z "$json" ]; then
+    _usage "Usage: _issue json"
+    return 1
+  fi
+  # set dns env
+  `echo $json | jq -r .dns_env_shell`
+  dns_type=`echo $json |jq -r .dns_type `
+ 
+  if [ -z "$dns_type" ]; then
+    _usage "dns_type is Required"
+    return 1
+  fi
+
+  for row in $(echo "${json}" | jq -r '.domains[]'); do
+      _jq() {
+       echo ${row}
+      }
+    domain=$(_jq '.[0]')
+    echo "issue $domain"
+    ~/.acme.sh/acme.sh --issue --dns $dns_type -d $domain
+    echo "issue $domain success"
+  done
+}
+
+_update_qingcloud_lb() {
+  json="$1"
+  echo $json
+  if [ -z "$json" ]; then
+    _usage "Usage: _issue json"
+    return 1
+  fi
+  # delete qingcloud config file
+  echo "clear qingcloud config file $QINGCLOUD_CONFIG"
+  rm -fr $QINGCLOUD_CONFIG
+
+  qy_access_key_id=`echo $json |jq -r .qy_access_key_id `
+  qy_secret_access_key=`echo $json |jq -r .qy_secret_access_key `
+  zone=`echo $json |jq -r .zone `
+
+  # create qingcloud config file
+  echo "create qingcloud config file $QINGCLOUD_CONFIG"
+
+  echo "qy_access_key_id: '$qy_access_key_id'" >> $QINGCLOUD_CONFIG
+  echo "qy_secret_access_key: '$qy_secret_access_key'" >> $QINGCLOUD_CONFIG
+  echo "zone: '$zone'" >> $QINGCLOUD_CONFIG
+
+  lb=`echo $json |jq -r .loadbalance_id `
+  lbl=`echo $json |jq -r .loadbalance_listener_id `
+
+  certificate_id_list=""
+
+  for row in $(echo "${json}" | jq -r '.domains[]'); do
+      _jq() {
+       echo ${row}
+      }
+    domain=$(_jq '.[0]')
+    echo $domain
+    echo "create ssl key for domain $line"
+      keyFile=~/.acme.sh/$domain/$domain.key
+      fullchainFile=~/.acme.sh/$domain/fullchain.cer
+      name=`date +%Y%m%d`
+      # create server certificate, remeber id
+      response=`qingcloud iaas create-server-certificate -N $domain-$name -C "$fullchainFile" -K "$keyFile" -f $QINGCLOUD_CONFIG`
+      echo $response
+      certificate_id=`echo $response | jq -r .server_certificate_id`
+      certificate_id_list="${certificate_id_list},$certificate_id"
+      echo "create ssl key success for $domain"
+  done
+
+  certificate_id_list=`echo $certificate_id_list | cut -c 2-`
+
+  old_ssl_id=`qingcloud iaas describe-loadbalancer-listeners -l $lb -s $lbl -f $QINGCLOUD_CONFIG | jq -r '.loadbalancer_listener_set[0].server_certificate_id | join(",")'`
+
+  echo "set server_certificate_id $certificate_id_list to loadbalance $lb with loadbalance listener $lbl"
+  # update loadbalancer listener setting, apply setting
+  qingcloud iaas modify-loadbalancer-listener-attributes -s $lbl -S $certificate_id_list -f $QINGCLOUD_CONFIG
+  qingcloud iaas update-loadbalancers -l $lb -f $QINGCLOUD_CONFIG
+
+  echo "old ssl id:$old_ssl_id"
+  echo "waiting for change..."
+  lb_status=`qingcloud iaas describe-loadbalancers -l $lb -f $QINGCLOUD_CONFIG | jq -r '.loadbalancer_set[0].status'`
+  timeout=0
+  while true ; do
+      timeout=$(($timeout+1))
+      if [ "$timeout" -gt 5 ]
+      then
+        break
+      fi
+      echo "loadbalancer status:$lb_status"
+      if [ "$lb_status" != "active" ]; then
+      sleep 3
+      lb_status=`qingcloud iaas describe-loadbalancers -l $lb -f $QINGCLOUD_CONFIG | jq -r '.loadbalancer_set[0].status'`
+      else
+          break;
+      fi
+  done
+
+  echo "delete old ssl id:$old_ssl_id"
+  qingcloud iaas delete-server-certificates -s $old_ssl_id -f $QINGCLOUD_CONFIG
+
+  # delete qingcloud config file
+  echo "delete qingcloud config file $QINGCLOUD_CONFIG"
+  rm -fr $QINGCLOUD_CONFIG
+}
+
+main() {
+config_file=""
+while [ ${#} -gt 0 ]; do
+  case "${1}" in
+    --help | -h)
+      showhelp
+      return
+      ;;
+    --version | -v)
+      version
+      return
+      ;;
+    --config | -c)
+      config_file="$2"
+      shift
+      ;;
+    *)
+      _err "Unknown parameter : $1"
+      showhelp
+      return 1
+      ;;
+  esac
+  shift 1
+done
+
+# check deps
+_check_deps
 
 if [ -z "$config_file" ]; then
   echo "use default config: config.json"
@@ -83,96 +203,17 @@ else
   echo "use custom config: $config_file"
 fi
 
-# set dns env
+# load config
 json=`cat $config_file`
-#echo "$json"
-# set dns env
-`echo $json | jq -r .dns_env_shell`
-dns_type=`echo $json |jq -r .dns_type `
 
-for row in $(echo "${json}" | jq -r '.domains[]'); do
-    _jq() {
-     echo ${row}
-    }
-  domain=$(_jq '.[0]')
-  echo "issue $domain"
-  ~/.acme.sh/acme.sh --issue --dns $dns_type -d $domain
-  echo "issue $domain success"
-done
+# issue domain
+_issue "$json"
 
+# update qingcloud loadbalancer settings
 QINGCLOUD_CONFIG=qingcloud_config.yaml
-# delete qingcloud config file
-echo "clear qingcloud config file $QINGCLOUD_CONFIG"
-rm -fr $QINGCLOUD_CONFIG
+export QINGCLOUD_CONFIG
+_update_qingcloud_lb "$json"
 
-qy_access_key_id=`echo $json |jq -r .qy_access_key_id `
-qy_secret_access_key=`echo $json |jq -r .qy_secret_access_key `
-zone=`echo $json |jq -r .zone `
-
-# create qingcloud config file
-echo "create qingcloud config file $QINGCLOUD_CONFIG"
-
-echo "qy_access_key_id: '$qy_access_key_id'" >> $QINGCLOUD_CONFIG
-echo "qy_secret_access_key: '$qy_secret_access_key'" >> $QINGCLOUD_CONFIG
-echo "zone: '$zone'" >> $QINGCLOUD_CONFIG
-
-lb=`echo $json |jq -r .loadbalance_id `
-lbl=`echo $json |jq -r .loadbalance_listener_id `
-
-certificate_id_list=""
-
-for row in $(echo "${json}" | jq -r '.domains[]'); do
-    _jq() {
-     echo ${row}
-    }
-  domain=$(_jq '.[0]')
-  echo $domain
-  echo "create ssl key for domain $line"
-    keyFile=~/.acme.sh/$domain/$domain.key
-    fullchainFile=~/.acme.sh/$domain/fullchain.cer
-    name=`date +%Y%m%d`
-    # create server certificate, remeber id
-    response=`qingcloud iaas create-server-certificate -N $domain-$name -C "$fullchainFile" -K "$keyFile" -f $QINGCLOUD_CONFIG`
-    echo $response
-  certificate_id=`echo $response | jq -r .server_certificate_id`
-    certificate_id_list="${certificate_id_list},$certificate_id"
-    echo "create ssl key success for $domain"
-done
-
-certificate_id_list=`echo $certificate_id_list | cut -c 2-`
-
-old_ssl_id=`qingcloud iaas describe-loadbalancer-listeners -l $lb -s $lbl -f $QINGCLOUD_CONFIG | jq -r '.loadbalancer_listener_set[0].server_certificate_id | join(",")'`
-
-echo "set server_certificate_id $certificate_id_list to loadbalance $lb with loadbalance listener $lbl"
-## update loadbalancer listener setting, apply setting
-qingcloud iaas modify-loadbalancer-listener-attributes -s $lbl -S $certificate_id_list -f $QINGCLOUD_CONFIG
-qingcloud iaas update-loadbalancers -l $lb -f $QINGCLOUD_CONFIG
-
-echo "old ssl id:$old_ssl_id"
-echo "waiting for change..."
-lb_status=`qingcloud iaas describe-loadbalancers -l $lb -f $QINGCLOUD_CONFIG | jq -r '.loadbalancer_set[0].status'`
-timeout=0
-while true ; do
-    timeout=$(($timeout+1))
-    if [ "$timeout" -gt 5 ]
-    then
-      break
-    fi
-    echo "loadbalancer status:$lb_status"
-    if [ "$lb_status" != "active" ]; then
-    sleep 3
-    lb_status=`qingcloud iaas describe-loadbalancers -l $lb -f $QINGCLOUD_CONFIG | jq -r '.loadbalancer_set[0].status'`
-    else
-        break;
-    fi
-done
-
-echo "delete old ssl id:$old_ssl_id"
-qingcloud iaas delete-server-certificates -s $old_ssl_id -f $QINGCLOUD_CONFIG
-
-# delete qingcloud config file
-echo "delete qingcloud config file $QINGCLOUD_CONFIG"
-rm -fr $QINGCLOUD_CONFIG
 }
 
 version() {
